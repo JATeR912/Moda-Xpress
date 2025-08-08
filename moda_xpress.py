d7b413ebe5bbe269598edb300d4d4301dd6b3742
JATeR912 <jater.code@gmail.com>
2025-08-08 19:52:42 -0400

    Se añade archivo .py

---
+++ moda_xpress.py
@@ -0,0 +1,25 @@
+#1.Apertura de archivo en modo lectura: Leer todo el inventario.
+
+
+#2.Apertura de archivo en modo escritura: Escribir datos de nuevos productos.
+
+
+#3.Apertura de archivo en modo "append": Agregar productos sin eliminar los datos existentes.
+
+
+#4.Apertura de archivo en modo lectura y escritura: Modificar y consultar productos existentes en el inventario.
+
+
+#5.Obtener atributos del archivo: Ver información sobre el archivo como el tamaño y la fecha de última modificación.
+
+
+#6.Leer un producto específico: Buscar por nombre o ID y mostrar los detalles del producto.
+
+
+#7.Modificar el nombre de un producto: Cambiar el nombre de un artículo en el inventario.
+
+
+#8.Eliminar un producto: Eliminar un producto del inventario.
+
+
+#9.Cerrar archivos: Asegurarse de cerrar correctamente el archivo después de cada operación.
